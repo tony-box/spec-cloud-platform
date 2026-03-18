a0aa1af29ca9dcd58707e048f7ef37cd694b3e4d
Tony Box <tobox@microsoft.com>
2026-03-18 16:28:27 -0700

    fix(sysprep): remove any todo file variants from customer release tags

--- sysprep.ps1
+++ sysprep.ps1
@@ -202,9 +202,8 @@ try {
     $vscodePath = Join-Path $StagingPath ".vscode"
     if (Test-Path $vscodePath) { Remove-Item $vscodePath -Recurse -Force }
 
-    # Remove internal development tracking files
-    $todoPath = Join-Path $StagingPath "to-do.md"
-    if (Test-Path $todoPath) { Remove-Item $todoPath -Force }
+    # Remove internal development tracking files (any todo/to-do variants)
+    Get-ChildItem $StagingPath -File -Filter "to*do*" | Remove-Item -Force
 
     Write-Host "[3/7] Internal content stripped"
 
